1071fab7dcf31902fe5cb1e4a576a09b11730dd8
Nikolas <nikolas.martinezrive6@mail.escuelaing.edu.co>
2026-02-05 08:25:23 -0500

    feat :  Synchronized inncesesarios removidos

--- src/main/java/co/eci/snake/core/Board.java
+++ src/main/java/co/eci/snake/core/Board.java
@@ -32,10 +32,10 @@ public final class Board {
   public int width() { return width; }
   public int height() { return height; }
 
-  public synchronized Set<Position> mice() { return new HashSet<>(mice); }
-  public synchronized Set<Position> obstacles() { return new HashSet<>(obstacles); }
-  public synchronized Set<Position> turbo() { return new HashSet<>(turbo); }
-  public synchronized Map<Position, Position> teleports() { return new HashMap<>(teleports); }
+  public Set<Position> mice() { return new HashSet<>(mice); }
+  public Set<Position> obstacles() { return new HashSet<>(obstacles); }
+  public Set<Position> turbo() { return new HashSet<>(turbo); }
+  public Map<Position, Position> teleports() { return new HashMap<>(teleports); }
 
   public  MoveResult step(Snake snake) {
       Objects.requireNonNull(snake, "snake");
@@ -45,12 +45,11 @@ public final class Board {
 
       boolean ateMouse;
       boolean ateTurbo;
-      boolean teleported;
+      boolean teleported = false;
 
       synchronized (lockStep) {
           if (obstacles.contains(next)) return MoveResult.HIT_OBSTACLE;
 
-          teleported = false;
           if (teleports.containsKey(next)) {
               next = teleports.get(next);
               teleported = true;
